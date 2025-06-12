fcba06b20e4d714cc26b54c567175139fdfc9dbf
Mickey <89617187+mickeysingh2012@users.noreply.github.com>
2025-06-11 20:28:06 -0400

    LordOfTheRingsCurrencyConverter
    Currency Converter App using SwiftUI 6 and Swift 6

---
+++ LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by user275984 on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
